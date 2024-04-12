f16f484dccb935d6cbbe9d99e50352f1cc709d24
appanikaushik <130453347+AyushiShukla1703@users.noreply.github.com>
2024-04-12 17:45:03 +0530

    bug

--- middleware/mailer.js
+++ middleware/mailer.js
@@ -1,5 +1,5 @@
 const nodemailer = require("nodemailer");
-const fs = require('node:fs');
+const fs = require('fs');
 const path = require("path");
 const mailer = (recipent,body) => {
     var transporter = nodemailer.createTransport({
