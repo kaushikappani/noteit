c72123b0050a2b041b2453793af2849f2ceff66a
appanikaushik <130453347+AyushiShukla1703@users.noreply.github.com>
2024-04-18 15:48:38 +0530

    Revert "stock updates"
    This reverts commit 98a9b569f4b26f4f1fb3e8fd40b4da01290d165e.

--- app.js
+++ app.js
@@ -10,11 +10,6 @@ const { Note } = require("./config/models");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser")
 
-const socket = require("socket.io");
-const { NseIndia } = require("stock-nse-india");
-const nseIndia = new NseIndia()
-
-
 app.use(bodyParser.urlencoded({
     limit: "50mb",
     extended: false
@@ -54,72 +49,6 @@ app.use(errorHandler)
 app.use(notFound)
 
 
-const server = app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
     console.log(`server running ${process.env.PORT}`)
 })
-
-const io = socket(server, {
-    cors: {
-        origin: ["http://localhost:3000", "https://coinwatch-nine.vercel.app"],
-        methods: ["GET", "POST"]
-    }
-});
-
-
-
-
-
-const symbolQuantityObject = {
-    "ARVIND": 20,
-    "DREAMFOLKS": 21,
-    "EXIDEIND": 40,
-    "INDHOTEL": 17,
-    "ITC": 40,
-    "JIOFIN": 227,
-    "KCP": 25,
-    "MOTHERSON": 110,
-    "NHPC": 51,
-    "PARKHOTELS": 9,
-    "PNB": 155,
-    "POWERGRID": 17,
-    "RECLTD": 50,
-    "SBIN": 22,
-    "SUZLON": 418,
-    "TATAMOTORS": 10,
-    "TATAPOWER": 82,
-    "TITAGARH": 48,
-    "UJJIVAN": 20,
-    "UJJIVANSFB": 152,
-    "VBL": 10
-};
-
-io.on("connection", (socket) => {
-
-    console.log(socket.id);
-
-    // Fetch data for fixed symbols, calculate total, and emit to client
-    const fetchDataAndSendTotal = async () => {
-        let total = 0;
-        for (const symbol of Object.keys(symbolQuantityObject)) {
-            try {
-                const data = await nseIndia.getEquityCorporateInfo(symbol);
-                const quantity = symbolQuantityObject[symbol];
-                total += parseFloat(data.priceInfo.change) * quantity;
-            } catch (error) {
-                console.error(`Error fetching data for ${symbol}: ${error}`);
-            }
-        }
-        io.to(socket.id).emit("totalPrice", total);
-    };
-
-    // Call fetchDataAndSendTotal when client connects and every 1 second
-    setInterval(fetchDataAndSendTotal, 1000);
-
-    // Handle disconnection
-    socket.on("disconnect", async () => {
-        console.log(`Client disconnected: ${socket.id}`);
-    });
-
-});
-
-
--- routes/StockService.js
+++
@@ -1,3 +0,0 @@
-const socket = require("socket.io");
-const { NseIndia } = require("stock-nse-india");
-const nseIndia = new NseIndia()
