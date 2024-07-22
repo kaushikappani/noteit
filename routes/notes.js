3112640348b085325c147568929636fa376a5daa
appanikaushik <appani.kaushik@bajajfinserv.in>
2024-07-22 08:40:53 +0530

    change in max output

--- routes/notes.js
+++ routes/notes.js
@@ -337,7 +337,7 @@ router.route("/:id/genai/summary").get(stockProtect,asyncHandler(async (req, res
             temperature: 0.9,
             topP: 0.9,
             topK: 64,
-            maxOutputTokens: 1000,
+            maxOutputTokens: 5000,
             responseMimeType: "text/plain",
         };
 
