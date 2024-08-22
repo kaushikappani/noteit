e74e383bb311a632e4cd310df8009bd37c007dc2
appanikaushik <appani.kaushik@bajajfinserv.in>
2024-08-22 19:38:05 +0530

    telegram bot redis

--- middleware/telegramAIBot.js
+++ middleware/telegramAIBot.js
@@ -3,28 +3,27 @@ const token = process.env.TELEGRAM_BOT_TOKEN_AI;
 const {
     GoogleGenerativeAI,
 } = require("@google/generative-ai");
+const client = require('./redis');
 const aibot = new TelegramBot(token, { polling: true });
+const util = require("util");
+const { telegramProtect } = require('./protect');
 
-let chatHistory = [];
 
-aibot.onText(/^\/clear/, async (msg) => {
-    const chatId = msg.chat.id;
-    chatHistory = [];
-    await aibot.sendMessage(chatId, "Chat History Cleared!");
-})
-aibot.on('message', async (msg) => {
+
+
+
+aibot.on('message',async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
     await aibot.sendChatAction(chatId, 'typing');
     try {
-        console.log(msg.chat.id)
         if (chatId === 1375808164) {
 
             const apiKey = process.env.GEMINI_API_KEY;
             const genAI = new GoogleGenerativeAI(apiKey);
             const model = genAI.getGenerativeModel({
                 model: "gemini-1.5-flash",
-                systemInstruction: "",
+                systemInstruction: "you are personal AI chat bot learn form the chat history about the person and respond",
             });
 
             const generationConfig = {
@@ -34,8 +33,11 @@ aibot.on('message', async (msg) => {
                 maxOutputTokens: 5000,
                 responseMimeType: "text/plain",
             };
-
+            const getAsync = util.promisify(client.lRange).bind(client);
+            let redisChatHistory = await getAsync(`chatHistory:${chatId}`, 0, -1);
             async function run() {
+                redisChatHistory = redisChatHistory || [];
+                const chatHistory = redisChatHistory.map((entry) => JSON.parse(entry));
                 const chatSession = model.startChat({
                     generationConfig,
                     // safetySettings: Adjust safety settings
@@ -43,23 +45,20 @@ aibot.on('message', async (msg) => {
                     history: [...chatHistory
                     ],
                 });
-
-
                 const result = await chatSession.sendMessage(text);
 
-                chatHistory.push({
+                const userMessage = {
                     role: "user",
-                    parts: [
-                        { text: text + "\n" },
-                    ],
-                });
+                    parts: [{ text: text  }],
+                };
+                await client.rpush(`chatHistory:${chatId}`, JSON.stringify(userMessage));
                 let output = result.response.text()
-                chatHistory.push({
+                
+                const aiMessage = {
                     role: "model",
-                    parts: [
-                        { text: output + "\n" },
-                    ],
-                });
+                    parts: [{ text: output  }],
+                };
+                await client.rpush(`chatHistory:${chatId}`, JSON.stringify(aiMessage));
 
                 await aibot.sendMessage(chatId, output);
 
@@ -71,5 +70,12 @@ aibot.on('message', async (msg) => {
         aibot.sendMessage(chatId, "Unable to process your message");
     }
 });
+
+
+aibot.onText(/^\/clear/, async (msg) => {
+    const chatId = msg.chat.id;
+    await client.del(`chatHistory:${chatId}`);
+    await aibot.sendMessage(chatId, "Chat History Cleared!");
+})
 
 module.exports = aibot
