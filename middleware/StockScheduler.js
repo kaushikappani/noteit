dd1bc1fd3bb5a6e8408954ac6b1321c347e4f2bb
appanikaushik <appani.kaushik@bajajfinserv.in>
2024-09-16 18:19:19 +0530

    new

--- middleware/StockScheduler.js
+++ middleware/StockScheduler.js
@@ -493,9 +493,9 @@ const triggerNotifications = async (req) => {
     // Retrieve all subscriptions from the set
     let subscriptions = await getAsync('notification_subs');
     // Send notifications to each subscription
-    for (const subscription of subscriptions) {
-      await sendNotification(JSON.parse(subscription), data);
-    }
+    subscriptions.forEach(subscription => {
+      sendNotification(JSON.parse(subscription), data);
+    });
   } catch (err) {
     console.error('Error retrieving subscriptions:', err);
   }
