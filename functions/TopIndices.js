6c9280a0196266724738805e31b9e081e517b3cc
appanikaushik <kaushikappani@gmail.com>
2024-10-30 10:07:10 +0530

    excepton handling

--- functions/TopIndices.js
+++ functions/TopIndices.js
@@ -3,9 +3,14 @@ const yahooFinance = require('yahoo-finance2').default;
 const getTopIndices =  async() => {
     let usIndices = ['^GSPC', '^DJI', '^IXIC', '^RUT']
     let indianIndices = ['^NSEI', '^BSESN', '^NSEBANK', '^NSEMDCP50', '^CNXIT']
-    
-    let usResponse = await yahooFinance.quote(usIndices);
-    let indianResponses =await yahooFinance.quote(indianIndices);
+    let indianResponses = [];
+    let usResponse = [];
+    try {
+         usResponse = await yahooFinance.quote(usIndices);
+        indianResponses = await yahooFinance.quote(indianIndices);
+    } catch (e) {
+        console.log(e);
+   }
 
     return { us: usResponse, indian: indianResponses };
 }
