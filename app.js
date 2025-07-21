efc3c30f17a602af595c9ec3cf4db1ac07f6fed7
Appani Kaushik <58048398+kaushikappani@users.noreply.github.com>
2025-07-21 10:18:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const aiGptRoutes = require("./routes/aigpt");
 
 require('./functions/Scheduler');
 
-require("./middleware/telegramBot");
+// require("./middleware/telegramBot");
 // if (process.env.NODE_ENV === "production") {
 //     require("./middleware/telegramBot");
 //     require("./middleware/telegramAIBot");
